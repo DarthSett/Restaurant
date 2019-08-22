5e30eaf8f8c272edacbfdb4c4a1cbb98e9870f8f
Sourav Sett <sourav241196@gmail.com>
2019-08-22 18:44:59 +0530

    wrote config file for CircleCi

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/restaurant/pkg/database/mysql"
 	"github.com/restaurant/pkg/server"
+	"os"
 )
 
 func main() {
+	port:=os.Getenv("PORT")
 	db := mysql.NewMySqlDB("db4free.net","hcs123","hcs12345","3306","restaurants_123")
 	defer db.Close()
 	s := server.NewServer(db)
-	s.Start(":4000")
+	s.Start(":"+port)
 }
