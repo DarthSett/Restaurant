f7bd780f2109034de18831768b13d401cc729104
Sourav Sett <sourav241196@gmail.com>
2019-08-27 17:40:09 +0530

    gofmt run

--- pkg/controller/UserControllers.go
+++ pkg/controller/UserControllers.go
@@ -230,8 +230,8 @@ func (u *UserController) UserUpdate(c *gin.Context) {
 		c.AbortWithError(http.StatusInternalServerError, fmt.Errorf("error getting id from claims: "+err.Error()))
 	}
 
-	rank,_ = strconv.Atoi(fmt.Sprintf("%v",claims["rank"]))
-	adder,_ = strconv.Atoi(fmt.Sprintf("%v",claims["id"]))
+	rank, _ = strconv.Atoi(fmt.Sprintf("%v", claims["rank"]))
+	adder, _ = strconv.Atoi(fmt.Sprintf("%v", claims["id"]))
 
 	if flag == 1 {
 		upd, err := bcrypt.GenerateFromPassword([]byte(update), 5)
@@ -248,11 +248,11 @@ func (u *UserController) UserUpdate(c *gin.Context) {
 		} else {
 			c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("admin is not the adder of this user"))
 		}
-		err = u.UpdateUser(id,update,flag)
+		err = u.UpdateUser(id, update, flag)
 	} else if user.Adder == adder && user.AdderRole == rank {
-		err = u.UpdateUser(id,update,flag)
+		err = u.UpdateUser(id, update, flag)
 	} else {
-		c.AbortWithError(http.StatusUnauthorized,fmt.Errorf("admin is not the adder of this user"))
+		c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("admin is not the adder of this user"))
 	}
 	c.Writer.Write([]byte("User Updated"))
 }
