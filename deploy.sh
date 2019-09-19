229de124ff32a155a0e8699715435a410c5d2b48
Sourav Sett <sourav241196@gmail.com>
2019-09-19 16:32:42 +0530

    Added the Customer Service

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-cd ./Res_Man_MicroService
 docker login --username=sourav@hotcocoasoftware.com --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app guarded-sierra-83575
 heroku container:release web --app guarded-sierra-83575
